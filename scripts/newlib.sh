d92646f613465e286ad88367a0a9d16898e347d7
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2023-06-13 11:09:25 +0900

    Enabled command substitution with environment variables.

--- scripts/newlib.sh
+++ scripts/newlib.sh
@@ -66,11 +66,11 @@ ${SRC_DIR}/${NEWLIB_DIR}/configure \
     --prefix=${INSTALL_DIR} \
     --target=${TARGET} \
 
-make -j${NUM_PROC} 2<&1 | tee build.newlib.1.log
+${MAKE} -j${NUM_PROC} 2<&1 | tee build.newlib.1.log
 if [ ${PIPESTATUS[0]} -ne 0 ]; then
 	exit 1;
 fi
-make install | tee build.newlib.2.log
+${MAKE} install | tee build.newlib.2.log
 if [ ${PIPESTATUS[0]} -ne 0 ]; then
 	exit 1;
 fi
